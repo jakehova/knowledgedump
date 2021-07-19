29d1480102461ca12d76e2f28c7ba43131d6989c
Jakehova <jacobcheriathundam@gmail.com>
2021-07-19 12:53:44 -0400

    update

--- tech/blockchain/ethereum/programming/_sample/arb-bot/index.js
+++ tech/blockchain/ethereum/programming/_sample/arb-bot/index.js
@@ -8,6 +8,8 @@ const web3 = new Web3(
   new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
 
+web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
+
 const kyber = new web3.eth.Contract(
   abis.kyber.kyberNetworkProxy,
   addresses.kyber.kyberNetworkProxy
